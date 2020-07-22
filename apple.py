353789c66558af312d4a54f521e781df9090a5ba
AmalPrakash <pkamalprakash98@gmail.com>
2020-07-23 03:41:15 +0530

    apple

---
+++ apple.py
@@ -0,0 +1,15 @@
+import random
+form config import config
+# class is used to represent apple as a combined object of X and Y coordinates..
+
+
+class Apple():
+
+    #When an instance is created, it runs setNewLocation
+    def __init__(self):
+        self.setNewLocation()
+    #sets new location for the apple 
+    def setNewLocation(self):
+        self.x = random.randint(0, Config.CELLWIDTH - 1)
+        self.y = random.randint(0, Config.CELLHEIGHT - 1)
+
