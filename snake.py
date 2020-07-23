5be50cb679f8c7b0dd04a2f9509e41aa33d32252
AmalPrakash <pkamalprakash98@gmail.com>
2020-07-23 20:13:46 +0530

    Final

--- snake.py
+++ snake.py
@@ -1,12 +1,14 @@
 from config import Config
 import random
+
+
 class Snake():
     UP = 'up'
     DOWN = 'down'
     LEFT = 'left'
     RIGHT = 'right'
     HEAD = 0
-    
+
     def __init__(self):
         # Starting coordinates --> 5 cells away from x and y
         self.x = random.randint(5, Config.CELLWIDTH - 6)
@@ -18,5 +20,28 @@ class Snake():
         # This is the snake
         self.sCoords = [{'x': self.x, 'y': self.y},
                         {'x': self.x - 1, 'y': self.y},
-                        {'x':self.x-2,'y':self.y}]
-        
+                        {'x': self.x - 2, 'y': self.y}]
+
+    def update(self, apple):
+        if self.sCoords[self.HEAD]['x'] == apple.x and self.sCoords[self.HEAD]['y'] == apple.y:
+            apple.setNewLocation()
+        else:
+            del self.sCoords[-1]  # removes the tail
+
+        if self.direction == self.UP:
+            newHead = {'x': self.sCoords[self.HEAD]['x'],
+                       'y': self.sCoords[self.HEAD]['y'] - 1}
+
+        elif self.direction == self.RIGHT:
+            newHead = {'x': self.sCoords[self.HEAD]['x']+1,
+                       'y': self.sCoords[self.HEAD]['y']}
+
+        elif self.direction == self.DOWN:
+            newHead = {'x': self.sCoords[self.HEAD]['x'],
+                       'y': self.sCoords[self.HEAD]['y'] + 1}
+
+        elif self.direction == self.LEFT:
+            newHead = {'x': self.sCoords[self.HEAD]['x']-1,
+                       'y': self.sCoords[self.HEAD]['y']}
+
+        self.sCoords.insert(0, newHead)
